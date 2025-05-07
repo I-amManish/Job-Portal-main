2af7195423f9c12bdbd220aa378350f4a4b60b7e
Manish <148551097+I-amManish@users.noreply.github.com>
2025-05-07 18:51:24 +0530

    Job Portal Main

---
+++ vcbackend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+
+
+app.listen(5001, () => {
+    console.log("server is running at port 5001")
+})
